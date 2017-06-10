1c55ca9b09e59e3be54a3c1fcf3082b3e08711cd
gc373 <leonardo.kid21@gmail.com>
2017-06-10 16:03:02 +0900

    catch error when favorite/create

--- nodefiles/autofav.js
+++ nodefiles/autofav.js
@@ -70,7 +70,10 @@ function run(){
 run();
 
 function createFav(favId){
-		client.post('favorites/create',{id : favId},function(){
-				console.log("<3 <3 <3 <3\r\n<3 fav it! <3\r\n<3 <3 <3 <3");
+		client.post('favorites/create',{id : favId}.then( function (tweet) {
+			console.log("<3 <3 <3 <3\r\n<3 fav it! <3\r\n<3 <3 <3 <3");
+		})
+			.catch(function (error) {
+				throw error;
 		})
 }

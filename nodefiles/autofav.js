24ec9513bc6c4f2946fd440959bcb87fdce91a1c
gc373 <leonardo.kid21@gmail.com>
2017-06-10 16:33:12 +0900

    revert

--- nodefiles/autofav.js
+++ nodefiles/autofav.js
@@ -70,10 +70,7 @@ function run(){
 run();
 
 function createFav(favId){
-		client.post('favorites/create',{id : favId}).then( function (tweet) {
-			console.log("<3 <3 <3 <3\r\n<3 fav it! <3\r\n<3 <3 <3 <3");
-		})
-			.catch(function (error) {
-				throw error;
+		client.post('favorites/create',{id : favId},function(){
+				console.log("<3 <3 <3 <3\r\n<3 fav it! <3\r\n<3 <3 <3 <3");
 		})
 }

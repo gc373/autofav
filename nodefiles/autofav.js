37dd27db7af439bd527bcc69fd20ab5c9e178ae9
gc373 <leonardo.kid21@gmail.com>
2017-06-10 23:31:11 +0900

    use space (instead of tab) && Improve log && clean

--- nodefiles/autofav.js
+++ nodefiles/autofav.js
@@ -8,69 +8,73 @@ const TARGET_ID           = process.env.TARGET_ID;
 let client;
 
 if(CONSUMER_KEY    == undefined || CONSUMER_SECRET    == undefined || ACCESS_TOKEN_KEY   == undefined || ACCESS_TOKEN_SECRET== undefined) {
-	console.log('environment variable is not set')
-	return;
+  console.log('environment variable is not set')
+  return;
 } else {
-	client = new twitter({
-			consumer_key       : CONSUMER_KEY       ,
-			consumer_secret    : CONSUMER_SECRET    ,
-			access_token_key   : ACCESS_TOKEN_KEY   ,
-			access_token_secret: ACCESS_TOKEN_SECRET,
-	});
+  client = new twitter({
+      consumer_key       : CONSUMER_KEY       ,
+      consumer_secret    : CONSUMER_SECRET    ,
+      access_token_key   : ACCESS_TOKEN_KEY   ,
+      access_token_secret: ACCESS_TOKEN_SECRET,
+  });
 }
 
-function run(){
-		client.stream('user' , function(stream){
-				stream.on('data', function(tweet){
-						if(tweet.text && !tweet.retweeted_status) {
-								if( TARGET_ID == undefined ? tweet.user.screen_name != MY_ID && !tweet.retweeted_status : tweet.user.screen_name == TARGET_ID && !tweet.retweeted_status) {
-										 createFav(tweet.id_str)
-								 }
-								console.log("------------------------------ TWEEEEEEEEEEEEEET ------------------------------")
-								console.log(tweet.id);
-								console.log(tweet.user.name + " / " + tweet.user.screen_name);
-								console.log(tweet.text+"\r\n");
-						}
-						if(tweet.retweeted_status) {
-								console.log(" ------------------- RT ------------------- ")
-								console.log(tweet.id)
-								console.log(tweet.user.name + "@ / " + tweet.user.screen_name);
-								console.log(tweet.text+"\r\n")
-						}
-				})
-				stream.on('delete' , function(deleteData){
-						console.log(" ------------------- DELETE TWEET ------------------- \r\n" + JSON.stringify(deleteData))
-				})
+function run() {
+    client.stream('user' , function(stream) {
+        stream.on('data', function(tweet) {
+            if(tweet.text && !tweet.retweeted_status) {
+                if( TARGET_ID == undefined ? tweet.user.screen_name != MY_ID && !tweet.retweeted_status : tweet.user.screen_name == TARGET_ID && !tweet.retweeted_status) {
+                     fav_create(tweet.id_str)
+                 }
+                console.log("------------------------------ TWEEEEEEEEEEEEEET ------------------------------")
+                console.log(tweet.id);
+                console.log(tweet.user.name + " / " + tweet.user.screen_name);
+                console.log(tweet.text+"\r\n");
+            }
+            if(tweet.retweeted_status) {
+                console.log(" ------------------- RT ------------------- ")
+                console.log(tweet.id)
+                console.log(tweet.user.name + "@ / " + tweet.user.screen_name);
+                console.log(tweet.text+"\r\n")
+            }
+        })
+        stream.on('delete' , function(deleteData){
+            console.log(" ------------------- DELETE TWEET ------------------- \r\n" + JSON.stringify(deleteData,'','\t'))
+        })
 
-				stream.on('error', function(e){
-						console.log(e);
-						setTimeout( function(){
-								stream.destroy();
-								run();
-						}, 1000 * 20);
-				})
+        stream.on('error', function(e) {
+            console.log(e);
+            setTimeout( function() {
+              stream.destroy();
+              run();
+            }, 1000 * 20);
+        })
 
-				stream.on('end', function(e){
-						console.log(e);
-						setTimeout( function(){
-								stream.destroy();
-								run();
-						}, 1000 * 20);
-				})
+        stream.on('end', function(e) {
+            console.log(e);
+            setTimeout( function() {
+              stream.destroy();
+              run();
+            }, 1000 * 20);
+        })
 
-				stream.on('disconnect', function(e){
-						 console.log(e);
-						 setTimeout( function(){
-								 stream.destroy();
-								 run();
-						 }, 1000 * 20);
-				 })
-		})
+        stream.on('disconnect', function(e) {
+          console.log(e);
+          setTimeout( function() {
+            stream.destroy();
+            run();
+          }, 1000 * 20);
+         })
+    })
 }
 run();
 
-function createFav(favId){
-		client.post('favorites/create',{id : favId},function(){
-				console.log("<3 <3 <3 <3\r\n<3 fav it! <3\r\n<3 <3 <3 <3");
-		})
+function fav_create(favId){
+    client.post('favorites/create',{id : favId},function(error, tweet, response) {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log("<3 <3 <3 <3\r\n<3 fav it! <3\r\n<3 <3 <3 <3");
+        }
+    })
 }
